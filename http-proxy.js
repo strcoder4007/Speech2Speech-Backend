7bed3615e4c06274221a5dd0cd6be07306769005
shubham4579 <shubham@data-hat.com>
2025-06-05 22:22:29 +0530

    Adds http proxy

---
+++ http-proxy.js
@@ -0,0 +1,17 @@
+const https = require('https');
+const httpProxy = require('http-proxy');
+const fs = require('fs');
+
+// Create proxy server to forward to your app
+const proxy = httpProxy.createProxyServer({ target: 'http://localhost:8001' });
+
+const server = https.createServer({
+  key: fs.readFileSync('./ssl/key.pem'),
+  cert: fs.readFileSync('./ssl/cert.pem')
+}, (req, res) => {
+  proxy.web(req, res);
+});
+
+server.listen(3443, () => {
+  console.log('🔐 HTTPS Proxy running on https://0.0.0.0:3443');
+});
